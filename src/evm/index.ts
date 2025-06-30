2de54e1feb509833424049e3f2e054d1083607d8
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-06-30 21:55:52 +0200

    feat: add ARB support (#1)
    * feat: upgrade checkpoint
    
    * feat: add ARB support

---
+++ src/evm/index.ts
@@ -0,0 +1,11 @@
+import Checkpoint, { evm } from '@snapshot-labs/checkpoint';
+import createWriters from './writers';
+import createConfig from './config';
+
+const ethIndexer = new evm.EvmIndexer(createWriters('eth'));
+const arb1Indexer = new evm.EvmIndexer(createWriters('arb1'));
+
+export function addEvmIndexers(checkpoint: Checkpoint) {
+  checkpoint.addIndexer('eth', createConfig('eth'), ethIndexer);
+  checkpoint.addIndexer('arb1', createConfig('arb1'), arb1Indexer);
+}
