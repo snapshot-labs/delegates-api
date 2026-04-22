1d2586c1a4e78f565ae2add4a6d91d1f560221fb
ChaituVR <yourchaitu@gmail.com>
2026-04-23 00:05:56 +0530

    fix: use HyperSync indexer for EVM networks

--- src/evm/index.ts
+++ src/evm/index.ts
@@ -2,10 +2,18 @@ import Checkpoint, { evm } from '@snapshot-labs/checkpoint';
 import createWriters from './writers';
 import createConfig from './config';
 
-const ethIndexer = new evm.EvmIndexer(createWriters('eth'));
-const arb1Indexer = new evm.EvmIndexer(createWriters('arb1'));
+const ethIndexer = process.env.HYPERSYNC_API_TOKEN
+  ? new evm.HyperSyncEvmIndexer(createWriters('eth'), {
+      apiToken: process.env.HYPERSYNC_API_TOKEN
+    })
+  : new evm.EvmIndexer(createWriters('eth'));
+const arb1Indexer = process.env.HYPERSYNC_API_TOKEN
+  ? new evm.HyperSyncEvmIndexer(createWriters('arb1'), {
+      apiToken: process.env.HYPERSYNC_API_TOKEN
+    })
+  : new evm.EvmIndexer(createWriters('arb1'));
 
 export function addEvmIndexers(checkpoint: Checkpoint) {
   checkpoint.addIndexer('eth', createConfig('eth'), ethIndexer);
-  // checkpoint.addIndexer('arb1', createConfig('arb1'), arb1Indexer);
+  checkpoint.addIndexer('arb1', createConfig('arb1'), arb1Indexer);
 }
